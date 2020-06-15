513c3e471ab7e27f2a3ee6576637f2d0d7466ff2
Victor Bejarano <alejandrobjrn4@gmail.com>
2020-06-15 00:17:43 -0500

    Users effects pt.2

--- src/app/store/effects/usuarios.effects.ts
+++ src/app/store/effects/usuarios.effects.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import * as usuariosActions from '../actions';
-import { mergeMap, map } from 'rxjs/operators';
+import { mergeMap, map, switchMap } from 'rxjs/operators';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Injectable()
 export class UsuariosEffects {
-  constructor(private actions$: Actions<any>) {}
+  constructor(
+    private actions$: Actions<any>,
+    public usuarioService: UsuarioService
+  ) {}
 
-  @Effect({dispatch: false})
+  @Effect()
   CargarUsuarios$ = this.actions$.pipe(
     ofType(usuariosActions.CARGAR_USUARIOS),
-    map((action) => {
-      console.log(action);
-      return action;
+    switchMap(() => {
+      return this.usuarioService
+        .getUsers()
+        .pipe(map((users) => new usuariosActions.CargarUsuariosSuccess(users)));
     })
   );
 }
